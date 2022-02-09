62acd58211b06bd20d1992bc2cfdf42ae8c49117
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-02-09 11:04:52 -0300

    página inicial ejs

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,7 @@ const express = require("express");
 const routes = express.Router();
 
 // rotas get -------------------------------------------------------------------
-routes.get("/", function (req, res) {
-  res.send("Rota de cadastro de post");
-});
+routes.get("/", (req, res) => res.render('index'));
 
 routes.get("/teste", function (req, res) {
   res.send("teste");
